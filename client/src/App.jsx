a2b8b103d1395b858690a943abbc210bfa9ce850
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-02-14 12:22:47 +0530

    Facilities section completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,6 +26,7 @@ function App() {
           <Route path="/login" element={<Login />} />
         </Routes>
         <Popular/>
+        <Facilities/>
       </BrowserRouter>
     </>
   )
--- client/src/components/Facilities/Facilities.jsx
+++ client/src/components/Facilities/Facilities.jsx
@@ -1,10 +1,68 @@
 import React from 'react'
 import "./Facilities.scss"
+import { BsPersonWorkspace } from "react-icons/bs";
+import { LuParkingCircle } from "react-icons/lu";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { RiWifiFill } from "react-icons/ri";
+import { MdElectricBolt } from "react-icons/md";
+import { GrSwim } from "react-icons/gr";
+import { CgGym } from "react-icons/cg";
+import { BsThreeDots } from "react-icons/bs";
+
 
 const Facilities = () => {
   return (
-    <div>
-      <h1>This is Facilities page</h1>
+    <div className='facilite-header'>
+
+      <div className="text-box">
+        <h2 className="header">We do our best facilities provide you</h2>
+
+        <div className="header-content">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis molestiae minima inventore laudantium asperioremos quam deserunt?</div>
+
+        <div className="btn-contact"><button>Contact Now</button></div>
+      </div>
+      <div className="icon-box">
+        <div className="icon-sm-box">
+          <BsPersonWorkspace />
+          <h3 className="sm-box-text">Private Workspace</h3>
+        </div>
+
+        <div className="icon-sm-box">
+          <LuParkingCircle />
+          <h3 className="sm-box-text">Parking Area</h3>
+        </div>
+
+        <div className="icon-sm-box">
+          <FaChalkboardTeacher />
+          <h3 className="sm-box-text">Breakfast</h3>
+        </div>
+
+
+        <div className="icon-sm-box">
+          <RiWifiFill />
+          <h3 className="sm-box-text">Free Wifi</h3>
+        </div>
+
+        <div className="icon-sm-box">
+          <MdElectricBolt />
+          <h3 className="sm-box-text">Free Electricity</h3>
+        </div>
+
+        <div className="icon-sm-box">
+          <GrSwim />
+          <h3 className="sm-box-text">Swimming Pool</h3>
+        </div>
+
+        <div className="icon-sm-box">
+          <CgGym />
+          <h3 className="sm-box-text">Exercise Space</h3>
+        </div>
+
+        <div className="icon-sm-box">
+          <BsThreeDots />
+          <h3 className="sm-box-text">Other Service</h3>
+        </div>
+      </div>
     </div>
   )
 }

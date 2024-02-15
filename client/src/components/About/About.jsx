8119936a70a62f0564024eff738b91e108e9a45e
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-02-15 15:24:57 +0530

    About section completed

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './About.scss'
+
+import aboutImg from '../../assets/aboutHistry.jpg'
 const About = () => {
   return (
-    <div>
-      <h1>This is About page</h1>
+    <div className='about-header'>
+      <div className="img-box">
+        <img src={aboutImg} alt="" />
+      </div>
+      <div className="text-box">
+        <h3 className="header">Discover Our Histry</h3>
+        <p className="text-content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam minus tenetur dolores! Saepe corporis libero qui ipsum natus vero non.
+        </p>
+        <p className="text-content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident ut eaque quas quasi odit! Est dolores facere quo  Provident ut eaque quas quasi odit! Est dolores facere quomaiores voluptate tenetur ea minus laudantium nesciunt?
+        </p>
+        <button className="explore-btn">Explore More</button>
+      </div>
     </div>
   )
 }

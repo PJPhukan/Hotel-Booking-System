0094f852dfae4bd0b9cc676e7042ecb8147ebc7b
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-04-14 12:46:13 +0530

    feat : "Hotel model added "

---
+++ server/src/models/room.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+const roomSchema = new mongoose.Schema(
+  {
+    hotelId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hotel",
+    },
+    hotelTypes: {
+      type: String,
+      required: true,
+    },
+    roomNo: {
+      type: Number,
+      required: true,
+    },
+    PricePerNight: {
+      type: Number,
+      required: true,
+    },
+    capacity:{
+        type:Number,
+        required:true
+    },
+    isBooked:{
+        type:Boolean,
+        default:false
+    }
+  },
+  {
+    timestamps: true,
+  }
+);

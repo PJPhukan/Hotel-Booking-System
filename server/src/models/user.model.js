cec2c2ddf56da4954b1497c6567da3c2ac14d576
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-04-14 12:17:52 +0530

    feat: "User model added "

---
+++ server/src/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+
+
+
+const User = mongoose.model("User", userSchema);
+export { User };
